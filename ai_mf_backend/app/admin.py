6b85db7e1a37e030163531659db7994d2ae642de
Kushagrarajsinghavisoft <157873268+Kushagrarajsinghavisoft@users.noreply.github.com>
2024-10-05 17:06:42 +0530

    [AMRS-26] - Added: Database_Schema

--- ai_mf_backend/app/admin.py
+++ ai_mf_backend/app/admin.py
@@ -1,18 +1,108 @@
 from django.contrib import admin
-from .models import UserLogs, UserManagement
+from .models import (
+                    Gender, MaritalStatus, Occupation, AnnualIncome, MonthlySavingCapacity, 
+                    InvestmentAmountPerYear, UserPersonalDetails, UserContactInfo, UserOTP, 
+                    UserFinancialDetails, Section, Question, Response, UserResponse,
+                    )
+
+@admin.register(Gender)
+class GenderAdmin(admin.ModelAdmin):
+    list_display = ('gender', 'add_date', 'update_date')
+    search_fields = ('gender',)
+    ordering = ('gender',)
 
 
-@admin.register(UserLogs)
-class UserLogsAdmin(admin.ModelAdmin):
-    list_display = ("mobile_number", "email_id", "last_access", "action")
-    search_fields = ("email_id", "mobile_number")
-    list_filter = ("action", "device_type")
-    ordering = ("-last_access",)
+@admin.register(MaritalStatus)
+class MaritalStatusAdmin(admin.ModelAdmin):
+    list_display = ('status', 'add_date', 'update_date')
+    search_fields = ('status',)
+    ordering = ('status',)
 
 
-@admin.register(UserManagement)
-class UserManagementAdmin(admin.ModelAdmin):
-    list_display = ("mobile_number", "email", "created_at")
-    search_fields = ("email", "mobile_number")
-    list_filter = ("updated_at",)
-    ordering = ("-created_at",)
+@admin.register(Occupation)
+class OccupationAdmin(admin.ModelAdmin):
+    list_display = ('occupation', 'add_date', 'update_date')
+    search_fields = ('occupation',)
+    ordering = ('occupation',)
+
+
+@admin.register(AnnualIncome)
+class AnnualIncomeAdmin(admin.ModelAdmin):
+    list_display = ('income_category', 'add_date', 'update_date')
+    search_fields = ('income_category',)
+    ordering = ('income_category',)
+
+
+@admin.register(MonthlySavingCapacity)
+class MonthlySavingCapacityAdmin(admin.ModelAdmin):
+    list_display = ('saving_category', 'add_date', 'update_date')
+    search_fields = ('saving_category',)
+    ordering = ('saving_category',)
+
+
+@admin.register(InvestmentAmountPerYear)
+class InvestmentAmountPerYearAdmin(admin.ModelAdmin):
+    list_display = ('investment_amount_per_year', 'add_date', 'update_date')
+    search_fields = ('investment_amount_per_year',)
+    ordering = ('investment_amount_per_year',)
+
+
+@admin.register(UserPersonalDetails)
+class UserPersonalDetailsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'date_of_birth', 'gender', 'marital_status', 'add_date', 'update_date')
+    search_fields = ('name',)
+    list_filter = ('gender', 'marital_status')
+    ordering = ('name',)
+
+
+@admin.register(UserContactInfo)
+class UserContactInfoAdmin(admin.ModelAdmin):
+    list_display = ('email', 'phone_number', 'add_date', 'update_date')
+    search_fields = ('email', 'phone_number')
+    ordering = ('email',)
+
+
+@admin.register(UserOTP)
+class UserOTPAdmin(admin.ModelAdmin):
+    list_display = ('user', 'otp', 'otp_valid', 'add_date', 'update_date')
+    search_fields = ('user__name', 'otp')
+    ordering = ('-add_date',)
+
+
+@admin.register(UserFinancialDetails)
+class UserFinancialDetailsAdmin(admin.ModelAdmin):
+    list_display = ('user', 'occupation', 'annual_income', 'monthly_saving_capacity', 'investment_amount_per_year', 'add_date', 'update_date')
+    search_fields = ('user__name',)
+    list_filter = ('occupation', 'annual_income', 'monthly_saving_capacity', 'investment_amount_per_year')
+    ordering = ('user',)
+
+
+@admin.register(Section)
+class SectionAdmin(admin.ModelAdmin):
+    list_display = ('section_name', 'add_date', 'update_date')
+    search_fields = ('section_name',)
+    ordering = ('section_name',)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('section', 'question', 'add_date', 'update_date')
+    search_fields = ('question',)
+    list_filter = ('section',)
+    ordering = ('section',)
+
+
+@admin.register(Response)
+class ResponseAdmin(admin.ModelAdmin):
+    list_display = ('question', 'section', 'response', 'add_date', 'update_date')
+    search_fields = ('response',)
+    list_filter = ('section', 'question')
+    ordering = ('question',)
+
+
+@admin.register(UserResponse)
+class UserResponseAdmin(admin.ModelAdmin):
+    list_display = ('user', 'question', 'response', 'section', 'add_date', 'update_date')
+    search_fields = ('user__name', 'question__question')
+    list_filter = ('section', 'question')
+    ordering = ('user',)
--- ai_mf_backend/app/models.py
+++ ai_mf_backend/app/models.py
@@ -10,81 +10,196 @@ def validate_mobile_number(value):
         )
 
 
-class UserLogs(models.Model):
-    """
-    Model for the ``user_logs`` table. This class will be used to maintain user entry logs.
-    """
+class Gender(models.Model):
+    gender = models.CharField(max_length=50)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
-    # initial
-    mobile_number = models.CharField(
+    class Meta:
+        db_table = "gender"
+    
+    def __str__(self):
+        return self.gender
+
+
+class MaritalStatus(models.Model):
+    status = models.CharField(max_length=50)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "marital_status"
+    
+    def __str__(self):
+        return self.status
+
+
+class Occupation(models.Model):
+    occupation = models.CharField(max_length=100)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "occupation"
+    
+    def __str__(self):
+        return self.occupation
+
+
+class AnnualIncome(models.Model):
+    income_category = models.CharField(max_length=100)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "annual_income"
+    
+    def __str__(self):
+        return self.income_category
+
+
+class MonthlySavingCapacity(models.Model):
+    saving_category = models.CharField(max_length=100)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "monthly_saving_capacity"
+    
+    def __str__(self):
+        return self.saving_category
+
+
+class InvestmentAmountPerYear(models.Model):
+    investment_amount_per_year = models.CharField(max_length=100)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "investment_amount_per_year"
+    
+    def __str__(self):
+        return self.investment_amount_per_year
+
+
+class UserPersonalDetails(models.Model):
+    user_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    date_of_birth = models.DateField()
+    gender = models.ForeignKey(Gender, on_delete=models.PROTECT)
+    marital_status = models.ForeignKey(MaritalStatus, on_delete=models.PROTECT)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "user_personal_details"
+    
+    def __str__(self):
+        return self.name
+
+class UserContactInfo(models.Model):
+    user = models.ForeignKey(UserPersonalDetails, on_delete=models.CASCADE)
+    email = models.EmailField(unique=True)
+    phone_number = models.CharField(
         max_length=10, validators=[validate_mobile_number], blank=True, null=True
     )
-    email_id = models.EmailField(max_length=255, db_index=True, blank=True, null=True)
-
-    # ip details sent by front end
-    ip_details = (
-        models.JSONField()
-    )  # To handle dynamic IP details similar to Mongo's DynamicField
-    device_type = models.CharField(max_length=100)
-
-    # time of the query response
-    last_access = models.DateTimeField()
-    ACTION_CHOICES = [
-        ("logged_in", "Logged In"),
-        ("logged_out", "Logged Out"),
-        ("invalid", "invalid_action"),
-    ]
-    action = models.CharField(
-        max_length=10,
-        choices=ACTION_CHOICES,
-        default="invalid",  # Set default to 'invalid'
-    )
+    password = models.CharField(max_length=100)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
 
     class Meta:
-        db_table = "user_logs"  # Equivalent to Mongo's collection
-        indexes = [
-            models.Index(fields=["email_id"]),
-        ]
-
-    def __str__(self):
-        return self.email_id
-
-
-from django.utils import timezone
-
-
-class UserManagement(models.Model):
-    """
-    Model for the ``user_management`` table. This class will be used to maintain users.
-    """
-
-    email = models.EmailField(
-        max_length=255, unique=True, db_index=True, blank=True, null=True
-    )
-    mobile_number = models.CharField(
-        max_length=10,
-        unique=True,
-        validators=[validate_mobile_number],
-        blank=True,
-        null=True,
-    )
-    password = models.CharField(max_length=255, blank=True)
-
-    # time of the query response
-    created_at = models.DateTimeField(default=timezone.now)
-    updated_at = models.DateTimeField(auto_now=True)
-    last_access = models.DateTimeField(null=True, blank=True)
-
-    # At the time of account creation
-    otp = models.IntegerField(null=True, blank=True)
-    otp_valid_till = models.DateTimeField(null=True, blank=True)
-
-    class Meta:
-        db_table = "user_management"  # Equivalent to Mongo's collection
+        db_table = "user_contact_info"
         indexes = [
             models.Index(fields=["email"]),
-            models.Index(fields=["mobile_number"]),
+            models.Index(fields=["phone_number"]),
         ]
 
     def __str__(self):
         return self.email
+
+
+class UserOTP(models.Model):
+    user = models.ForeignKey(UserPersonalDetails, on_delete=models.CASCADE)
+    otp = models.IntegerField()
+    otp_valid = models.DateTimeField()
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "user_otp"
+    
+    def __str__(self):
+        return f"OTP for {self.user.name}"
+
+
+class UserFinancialDetails(models.Model):
+    user = models.ForeignKey(UserPersonalDetails, on_delete=models.CASCADE)
+    occupation = models.ForeignKey(Occupation, on_delete=models.PROTECT)
+    annual_income = models.ForeignKey(AnnualIncome, on_delete=models.PROTECT)
+    monthly_saving_capacity = models.ForeignKey(MonthlySavingCapacity, on_delete=models.PROTECT)
+    investment_amount_per_year = models.ForeignKey(InvestmentAmountPerYear, on_delete=models.PROTECT)
+    regular_source_of_income = models.BooleanField(choices=[(True, 'Yes'), (False, 'No')], default=False)
+    lock_in_period_accepted = models.BooleanField(choices=[(True, 'Yes'), (False, 'No')], default=False)
+    investment_style = models.CharField(max_length=10, choices=[('SIP', 'SIP'), ('Lump-Sum', 'Lump-Sum')], default='SIP')
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "user_financial_details"
+    
+    def __str__(self):
+        return f"Financial Details for {self.user.name}"
+
+
+class Section(models.Model):
+    section_name = models.CharField(max_length=100)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "section"
+    
+    def __str__(self):
+        return self.section_name
+
+
+class Question(models.Model):
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    question = models.CharField(max_length=500)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "question"
+    
+    def __str__(self):
+        return self.question
+
+
+class Response(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    response = models.CharField(max_length=500)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "response"
+    
+    def __str__(self):
+        return self.response
+
+
+class UserResponse(models.Model):
+    user = models.ForeignKey(UserPersonalDetails, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    response = models.ForeignKey(Response, on_delete=models.CASCADE)
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    add_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "user_response"
+    
+    def __str__(self):
+        return f"Response by {self.user.name} for {self.question.question}"
