4fd840f09a2076f4bc025101c17306a633170d60
Kushagra Raj Singh <kushagra.singh@avisoft.io>
2024-10-14 18:35:42 +0530

    [AMRS-33]-Updated - integrated yahoo data pull

--- ai_mf_backend/api_application.py
+++ ai_mf_backend/api_application.py
@@ -51,6 +51,16 @@ from ai_mf_backend.models.v1.database.user_authentication import (
     UserLogs,
     UserManagement,
 )
+from ai_mf_backend.models.v1.database.mutual_fund import (
+    MutualFund,
+    HistoricalData,
+    PerformanceData,
+    TrailingReturn,
+    AnnualReturn,
+    FundData,
+    RiskStatistics,
+    FundOverview,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -114,6 +124,69 @@ class UserManagementAdmin(admin.ModelAdmin):
     list_filter = ("updated_at",)
     ordering = ("-created_at",)
 
+
+@admin.register(MutualFund)
+class MutualFundAdmin(admin.ModelAdmin):
+    list_display = ("scheme_name", "symbol", "net_asset_value", "date")
+    search_fields = ("scheme_name", "symbol")
+    list_filter = ("date",)
+
+
+@admin.register(HistoricalData)
+class HistoricalDataAdmin(admin.ModelAdmin):
+    list_display = ("fund", "date", "open", "close", "volume")
+    list_filter = ("fund", "date")
+    date_hierarchy = "date"
+
+
+@admin.register(PerformanceData)
+class PerformanceDataAdmin(admin.ModelAdmin):
+    list_display = (
+        "fund",
+        "morningstar_return_rating",
+        "ytd_return",
+        "average_return_5y",
+    )
+    list_filter = ("morningstar_return_rating",)
+
+
+@admin.register(TrailingReturn)
+class TrailingReturnAdmin(admin.ModelAdmin):
+    list_display = ("fund", "metric", "fund_return", "benchmark_return")
+    list_filter = ("metric", "fund")
+
+
+@admin.register(AnnualReturn)
+class AnnualReturnAdmin(admin.ModelAdmin):
+    list_display = ("fund", "year", "fund_return", "category_return")
+    list_filter = ("year", "fund")
+
+
+@admin.register(FundData)
+class FundDataAdmin(admin.ModelAdmin):
+    list_display = ("fund", "min_initial_investment", "min_subsequent_investment")
+
+
+@admin.register(RiskStatistics)
+class RiskStatisticsAdmin(admin.ModelAdmin):
+    list_display = ("fund", "period", "alpha", "beta", "sharpe_ratio")
+    list_filter = ("period", "fund")
+
+
+@admin.register(FundOverview)
+class FundOverviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "fund",
+        "category",
+        "fund_family",
+        "net_assets",
+        "ytd_return",
+        "yield_value",
+        "morningstar_rating",
+        "inception_date",
+    )
+    search_fields = ("fund__scheme_name", "category", "fund_family")
+    list_filter = ("category", "fund_family")
 
 
 # https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
--- ai_mf_backend/core/fastapi_blueprints.py
+++ ai_mf_backend/core/fastapi_blueprints.py
@@ -15,9 +15,13 @@ from ai_mf_backend.core.v1.api.user_data_capture.id_options import (
 from ai_mf_backend.core.v1.api.user_data_capture.user_personal_financial_detail import (
     router as user_personal_financial_detail_router_v1,
 )
+from ai_mf_backend.core.v1.api.yf_data_pull.yf_data_pull import (
+    router as yf_data_pull_router_v1,
+)
 connect_router.include_router(authentication_router_v1)
 connect_router.include_router(forget_password_router_v1)
 connect_router.include_router(otp_verification_router_v1)
 connect_router.include_router(id_options_router_v1)
 connect_router.include_router(user_personal_financial_detail_router_v1)
+connect_router.include_router(yf_data_pull_router_v1)
 
