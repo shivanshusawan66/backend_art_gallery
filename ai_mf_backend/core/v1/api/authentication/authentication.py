bc68da30362d95786bf10361459e4045ae13d5a8
Amay Thakur <amay.thakur@avisoft.io>
2024-10-28 11:35:14 +0530

    [AMRS-32] updated: User id mentioned in response data

--- ai_mf_backend/core/v1/api/authentication/authentication.py
+++ ai_mf_backend/core/v1/api/authentication/authentication.py
@@ -165,7 +165,8 @@ async def user_authentication_password(
                 message=f"Successfully logged in to the Dashboard",
                 data={
                     "token": jwt_token,
-                    "data": {"credentials": email if email else mobile_no},
+                    "data": {"credentials": email if email else mobile_no,
+                             "user_id": user_doc.user_id},
                 },
                 status_code=200,
             )
