4cb3668df5cf262897205f3d9c5b73c8791d56bb
Amay Thakur <amay.thakur@avisoft.io>
2024-11-06 18:15:26 +0530

    [AMRS-66]-Updated:Added validations for marital status model

--- ai_mf_backend/models/v1/database/user.py
+++ ai_mf_backend/models/v1/database/user.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.core.exceptions import ValidationError
 from phonenumber_field.validators import validate_international_phonenumber
 from ai_mf_backend.models.v1.database import SoftDeleteModel
+from ai_mf_backend.utils.v1.validation.status import validate_marital_status
 import re
 
 def validate_mobile_number(mobile_no: str) -> None:
@@ -33,7 +34,8 @@ class MaritalStatus(SoftDeleteModel):
 
     marital_status = models.CharField(
         max_length=50, 
-        unique=True
+        unique=True, 
+        validators=[validate_marital_status]
         )
     add_date = models.DateTimeField(
         auto_now_add=True
@@ -47,16 +49,8 @@ class MaritalStatus(SoftDeleteModel):
         verbose_name = "Marital Status"
         verbose_name_plural = "Marital Status"
     
-    def clean(self):
-        if not re.match(r'^[A-Za-z\s]+$', self.marital_status):
-            raise ValidationError(f"{self.marital_status} is not a valid marital status.")
-        
-        if not self.marital_status.strip():
-            raise ValidationError(f"Marital Status cannot be empty or only whitespace.")
     
-    def save(self, *args, **kwargs):
-        self.full_clean()
-        super().save(*args, **kwargs)
+
 
     def __str__(self):
         return self.marital_status
