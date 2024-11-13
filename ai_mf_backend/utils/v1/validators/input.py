3bec95273ed2a1886e676b5f38f5a7236486b04a
Amay Thakur <amay.thakur@avisoft.io>
2024-11-13 10:38:15 +0530

    [AMRS-105] - Updated:Made changes required for correct response in resend otp api along with formatting

--- ai_mf_backend/utils/v1/validators/input.py
+++ ai_mf_backend/utils/v1/validators/input.py
@@ -4,9 +4,6 @@ import re
 from django.core.exceptions import ValidationError
 
 
-
-
-
 def validate_number_dash_number(income_category):
     if not re.match(r"^\d+\s*-\s*\d+$", income_category):
         raise ValidationError(f"{income_category} is not a valid  category.")
