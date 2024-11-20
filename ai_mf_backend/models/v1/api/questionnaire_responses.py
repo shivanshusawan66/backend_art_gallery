86a80ca640a654a0326d2aa5efdb494fbfdf98fc
Anchit Shrivastava <anchit@avisoft.io>
2024-11-20 16:11:10 +0530

    [AMRS-117] - Updated: Saving responses temporarily is not required anymore

--- ai_mf_backend/models/v1/api/questionnaire_responses.py
+++ ai_mf_backend/models/v1/api/questionnaire_responses.py
@@ -1,17 +1,17 @@
 from pydantic import BaseModel
-from typing import List, Optional
+from typing import List
 
 from ai_mf_backend.models.v1.api import Response
 
 
 class ResponseItem(BaseModel):
     question_id: int
-    response: str  # Ensure this attribute exists and is properly defined.
+    response_id: int  # Ensure this attribute exists and is properly defined.
 
 
 class SubmitQuestionnaireRequest(BaseModel):
     user_id: int
-    section_id: Optional[int] = None
+    section_id: int
     responses: List[ResponseItem]  # A list of ResponseItem objects
 
 
