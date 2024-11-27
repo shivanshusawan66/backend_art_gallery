86c0cb94b9318fb848dd4d80d73886ab1f640242
sahil singh <sahilsingh@sahils-MacBook-Air.local>
2024-11-27 21:51:37 +0530

    [AMRS-195] - Updated : unique constraint in questionaire model ,fixed questionnaire database populating script

--- ai_mf_backend/models/v1/database/questions.py
+++ ai_mf_backend/models/v1/database/questions.py
@@ -4,7 +4,7 @@ from ai_mf_backend.models.v1.database import SoftDeleteModel
 
 
 class Section(SoftDeleteModel):
-    section = models.CharField(max_length=100)
+    section = models.CharField(max_length=100,unique=True)
     add_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
@@ -21,7 +21,7 @@ class Question(SoftDeleteModel):
     section = models.ForeignKey(
         Section, on_delete=models.SET_NULL, null=True, blank=True
     )
-    question = models.CharField(max_length=500)
+    question = models.CharField(max_length=500,unique=True)
     add_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
 
