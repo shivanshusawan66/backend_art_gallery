fa80fbb0380b0c6c9f7f63957e0734b9c202f4f1
sahilsingh-avisoft <sahil.singh@avisoft.io>
2024-11-27 16:43:18 +0530

    [AMRS-190] : Updated : updated pydantic and black formatted .

--- ai_mf_backend/core/v1/api/display_mf_data/display_all_mf_columns.py
+++ ai_mf_backend/core/v1/api/display_mf_data/display_all_mf_columns.py
@@ -1,10 +1,9 @@
 from asgiref.sync import sync_to_async
-from fastapi import APIRouter, Request, Response
 from ai_mf_backend.core.v1.api import limiter
+from fastapi import APIRouter, Request, Response
 from ai_mf_backend.config.v1.api_config import api_config
 from ai_mf_backend.models.v1.database.mutual_fund import Reference
 from ai_mf_backend.models.v1.api.display_all_mf_columns import (
-    ColumnModel,
     ErrorResponse,
     SuccessResponse,
 )
@@ -14,7 +13,7 @@ router = APIRouter()
 
 @limiter.limit(api_config.REQUEST_PER_MIN)
 @router.get(
-    "/references/",
+    "/mutual_funds_all_columns/",
     response_model=SuccessResponse,
     responses={200: {"model": SuccessResponse}, 404: {"model": ErrorResponse}},
 )
@@ -25,32 +24,23 @@ async def get_references_column(request: Request, response: Response):
         )
 
         if column_names:
-            columns = [ColumnModel(column_name=name) for name in column_names]
-
-            success_response = SuccessResponse(
-                status=True,
-                status_code=200,
-                message="Successfully retrieved references columns",
-                columns=columns,
-            )
             response.status_code = 200
-            return success_response
-
-        error_response = ErrorResponse(
-            status=False,
-            status_code=404,
-            message="No references found",
-            error_details="The references columns is empty",
-        )
-        response.status_code = 404
-        return error_response
+            return SuccessResponse(
+                status=True,
+                message="Successfully retrieved references columns",
+                columns=column_names,
+                status_code=200,
+            )
+        else:
+            response.status_code = 404
+            return ErrorResponse(
+                status=False, message="No  mutualfunds columns  found", status_code=404
+            )
 
     except Exception as e:
-        error_response = ErrorResponse(
-            status=False,
-            status_code=500,
-            message="Failed to retrieve references columns",
-            error_details=str(e),
-        )
         response.status_code = 500
-        return error_response
+        return ErrorResponse(
+            status=False,
+            message="Failed to retrieve mutualfunds columns",
+            status_code=500,
+        )
--- ai_mf_backend/models/v1/api/display_all_mf_columns.py
+++ ai_mf_backend/models/v1/api/display_all_mf_columns.py
@@ -1,23 +1,18 @@
 from pydantic import BaseModel, Field
-from typing import Dict, List, Optional
-
-
-class ColumnModel(BaseModel):
-    column_name: str = Field(..., description="Name of the column")
+from typing import List, Optional
 
 
 class SuccessResponse(BaseModel):
     status: bool = Field(default=True)
-    status_code: int = Field()
-    message: str = Field(default="Successfully retrieved references")
-    columns: List[ColumnModel]
+    message: str = Field(default="Successfully retrieved references columns")
+    columns: List[str]
+    status_code: int
 
 
 class ErrorResponse(BaseModel):
     status: bool = Field(default=False)
-    status_code: int
     message: str
-    error_details: Optional[str] = None
+    status_code: int
 
     class Config:
         orm_mode = True
