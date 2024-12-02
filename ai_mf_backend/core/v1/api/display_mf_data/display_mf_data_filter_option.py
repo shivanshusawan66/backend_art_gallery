094a72464d5c173b24e9defd23d0b6c84cd1aadd
Anchit Shrivastava <anchit@avisoft.io>
2024-12-02 17:00:20 +0530

    [AMRS-166] - Updated: Cleaned and optimised API code

--- ai_mf_backend/core/v1/api/display_mf_data/display_mf_data_filter_option.py
+++ ai_mf_backend/core/v1/api/display_mf_data/display_mf_data_filter_option.py
@@ -1,9 +1,10 @@
+import logging
+
 from fastapi import APIRouter, Request, Response
 
 from ai_mf_backend.core.v1.api import limiter
 
-
-from ai_mf_backend.utils.v1.constants import FILTER_OPTIONS
+from ai_mf_backend.utils.v1.constants import filter_option_object
 
 from ai_mf_backend.models.v1.api.display_mf_data_filter_option import (
     APIResponse,
@@ -12,6 +13,8 @@ from ai_mf_backend.models.v1.api.display_mf_data_filter_option import (
 
 from ai_mf_backend.config.v1.api_config import api_config
 
+
+logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
@@ -22,25 +25,34 @@ router = APIRouter()
 )
 async def mutual_funds_filter_options(request: Request, response: Response):
     try:
-        if FILTER_OPTIONS:
-            response.status_code = 200
-            return APIResponse(
-                status=True,
-                message="Filters options fetched successfully",
-                data=FundFamiliesResponseModel(
-                    fund_family=FILTER_OPTIONS["fund_families"],
-                    morningstar_ratings=FILTER_OPTIONS["morningstar_ratings"],
-                    min_initial_investment=FILTER_OPTIONS["min_initial_investments"],
-                ),
-                status_code=200,
-            )
-        else:
+        fund_families = filter_option_object.fund_families
+        morningstar_ratings = filter_option_object.morningstar_rating
+        min_initial_investments = filter_option_object.min_initial_investments
+        if not all(
+            [
+                fund_families,
+                morningstar_ratings,
+                min_initial_investments,
+            ]
+        ):
             response.status_code = 404
             return APIResponse(
                 status=False,
                 message="No data found",
+                data=dict(),
                 status_code=404,
             )
+        response.status_code = 200
+        return APIResponse(
+            status=True,
+            message="Filters options fetched successfully",
+            data=FundFamiliesResponseModel(
+                fund_family=fund_families,
+                morningstar_ratings=morningstar_ratings,
+                min_initial_investment=min_initial_investments,
+            ),
+            status_code=200,
+        )
 
     except Exception as e:
         response.status_code = 500
--- ai_mf_backend/utils/v1/constants.py
+++ ai_mf_backend/utils/v1/constants.py
@@ -1,38 +1,44 @@
 from ai_mf_backend.models.v1.database.mutual_fund import FundOverview, FundData
 
 
-def convert_morningstar_rating(rating: str) -> int:
-    """Converts a Morningstar rating string to an integer based on its length."""
-    return len(rating) if rating else 0
+class MFFilterOptions:
+    def __init__(self):
+        self.fund_families = None
+        self.morningstar_rating = None
+        self.min_initial_investments = None
 
+    @staticmethod
+    def convert_morningstar_rating(rating: str) -> int:
+        """Converts a Morningstar rating string to an integer based on its length."""
+        return len(rating) if rating else 0
 
-def precompute_filter_options():
-    """Precomputes filter options by querying the database with optimized field selection."""
-    fund_families = sorted(
-        FundOverview.objects.only("fund_family")
-        .values_list("fund_family", flat=True)
-        .distinct()
-    )
-    morningstar_ratings = sorted(
-        map(
-            convert_morningstar_rating,
-            FundOverview.objects.only("morningstar_rating")
-            .values_list("morningstar_rating", flat=True)
-            .distinct(),
+    def compute_filter_options(self):
+        """Precomputes filter options by querying the database with optimized field selection."""
+        fund_families = sorted(
+            FundOverview.objects.only("fund_family")
+            .values_list("fund_family", flat=True)
+            .distinct()
         )
-    )
-    min_initial_investments = sorted(
-        float(i)
-        for i in FundData.objects.only("min_initial_investment")
-        .values_list("min_initial_investment", flat=True)
-        .distinct()
-    )
+        morningstar_ratings = sorted(
+            map(
+                self.convert_morningstar_rating,
+                FundOverview.objects.only("morningstar_rating")
+                .values_list("morningstar_rating", flat=True)
+                .distinct(),
+            )
+        )
+        min_initial_investments = sorted(
+            float(i)
+            for i in FundData.objects.only("min_initial_investment")
+            .values_list("min_initial_investment", flat=True)
+            .distinct()
+        )
 
-    return {
-        "fund_families": fund_families,
-        "morningstar_ratings": morningstar_ratings,
-        "min_initial_investments": min_initial_investments,
-    }
+        self.fund_families = fund_families
+        self.morningstar_rating = morningstar_ratings
+        self.min_initial_investments = min_initial_investments
+
+        return self
 
 
-FILTER_OPTIONS = precompute_filter_options()
+filter_option_object = MFFilterOptions().compute_filter_options()
