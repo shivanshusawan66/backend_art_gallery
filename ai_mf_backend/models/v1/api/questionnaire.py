c503424bfabe01ddb744deb68a939d05c2dc60b9
abhijeetranaabisoft <abhijeet.rana@avisoft.io>
2025-03-31 16:38:07 +0530

    feature/AMRS-388/Backend-Adding-non_answered-questions-in-response-of-section-completion-status

--- ai_mf_backend/models/v1/api/questionnaire.py
+++ ai_mf_backend/models/v1/api/questionnaire.py
@@ -54,6 +54,7 @@ class SectionCompletionStatus(BaseModel):
     section_id: int
     section_name: str
     answered_questions: int
+    non_answered_questions:int
     total_questions: int
     completion_rate: int
 
