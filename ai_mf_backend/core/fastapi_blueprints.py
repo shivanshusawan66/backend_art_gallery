6b2a1305b745b622979a1f45c99c40c09ade4e93
Shivanshu Sawan <shivanshu.sawan@avisoft.io>
2025-04-18 12:55:52 +0530

    [AMRS-418] - Updated: added dummy api

--- ai_mf_backend/core/fastapi_blueprints.py
+++ ai_mf_backend/core/fastapi_blueprints.py
@@ -38,6 +38,10 @@ from ai_mf_backend.core.v1.api.mf_data.display_high_return_mutual_funds import (
     router as display_high_return_mutual_funds_router_v1,
 )
 
+from ai_mf_backend.core.v1.api.mf_data.display_mf_recommendations import (
+    router as mf_recommendations_router_v1,
+)
+
 # Blog Data Routers
 from ai_mf_backend.core.v1.api.blog.blog_data import (
     router as blog_data_router_v1,
@@ -74,6 +78,7 @@ connect_router.include_router(otp_verification_router_v1)
 connect_router.include_router(id_options_router_v1)
 connect_router.include_router(user_personal_financial_detail_router_v1)
 connect_router.include_router(display_user_form_responses_router_v1)
+connect_router.include_router(mf_recommendations_router_v1)
 
 connect_router.include_router(questionnaire_router_v1)
 connect_router.include_router(saving_response_router_v1)
@@ -92,6 +97,3 @@ connect_router.include_router(blog_comment_report_options_router_v1)
 connect_router.include_router(user_review_router_v1)
 
 connect_router.include_router(contact_message_router_v1)
-
-
-
---
+++ ai_mf_backend/core/v1/api/mf_data/display_mf_recommendations.py
@@ -0,0 +1,59 @@
+from fastapi import APIRouter, Query, Response, status, Request
+from ai_mf_backend.models.v1.api.mf_recommendations import MFRecommendationsResponse
+from ai_mf_backend.config.v1.api_config import api_config
+from ai_mf_backend.core.v1.api import limiter
+
+router = APIRouter()
+
+@limiter.limit(api_config.REQUEST_PER_MIN)
+@router.get("/mf_high_return_funds", response_model=MFRecommendationsResponse)
+async def get_high_return_funds(
+    request: Request,
+    response: Response,
+    page: int = Query(1, gt=0),
+    page_size: int = Query(api_config.DEFAULT_PAGE_SIZE, ge=1, le=api_config.MAX_PAGE_SIZE),
+):
+    response.status_code = status.HTTP_200_OK
+    return MFRecommendationsResponse(
+            status=True,
+            message="Success",
+            page=page,
+            nav_pages=10,
+            nav_data=10,
+            data=[{
+                "schemecode": 44234,
+                "s_name": "Nippon India Equity Hybrid Fund-Segregated Portfolio 1-(Q-IDCW)-Direct Plan",
+                "nav": 10.345,
+                "ytd_return": 126.760563380282,
+                "asset_type": "Hybrid"
+            },
+            {
+                "schemecode": 44234,
+                "s_name": "Nippon India Equity Hybrid Fund-Segregated Portfolio 1-(Q-IDCW)-Direct Plan",
+                "nav": 10.345,
+                "ytd_return": 126.760563380282,
+                "asset_type": "Hybrid"
+            },
+            {
+                "schemecode": 44228,
+                "s_name": "Nippon India Equity Hybrid Fund-Segregated Portfolio 1-(IDCW)",
+                "nav": 10.345,
+                "ytd_return": 126.415094339623,
+                "asset_type": "Hybrid"
+            },
+            {
+                "schemecode": 44228,
+                "s_name": "Nippon India Equity Hybrid Fund-Segregated Portfolio 1-(IDCW)",
+                "nav": 10.345,
+                "ytd_return": 126.415094339623,
+                "asset_type": "Hybrid"
+            },
+            {
+                "schemecode": 44229,
+                "s_name": "Nippon India Equity Hybrid Fund-Segregated Portfolio 1-(M-IDCW)",
+                "nav": 10.345,
+                "ytd_return": 126.262626262626,
+                "asset_type": "Hybrid"
+            }],
+            status_code=200,
+        )
